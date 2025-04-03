a2caaefadab4a79c71b7fe58c804c426edea575b
yuexiayiren159 <yuexiayiren159@gmail.com>
2025-04-03 16:11:53 +0800

    添加所有的d2l的ipynb文件

--- d2l-my/demo.py
+++ d2l-my/demo.py
@@ -1,19 +1,25 @@
-# mask = (torch.rand(X.shape) > dropout).float()
+# import torch
+# from torch import nn
+# from d2l import torch as d2l
 
-# mask = (torch.rand(X.shape) > dropout).float()
+# # 简洁实现
+# num_epochs, lr, batch_size = 10, 0.5, 256
+# dropout1, dropout2 = 0.2, 0.5
+# loss = nn.CrossEntropyLoss()
+# train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
 
-# mask = (torch.rand(X.shape) > dropout).float()
+# net = nn.Sequential(nn.Flatten(),nn.Linear(784,256),nn.ReLU(),
+#                    nn.Dropout(dropout1),nn.Linear(256,256),nn.ReLU(),
+#                    nn.Dropout(dropout2),nn.Linear(256,10))
 
-# mask = (torch.rand(X.shape) > dropout).float()
+# def init_weights(m):
+#     if type(m) == nn.Linear:
+#         nn.init.normal_(m.weight,std=0.01)
+    
+# net.apply(init_weights)
+# trainer = torch.optim.SGD(net.parameters(),lr=lr)
 
-# mask = (torch.rand(X.shape) > dropout).float()
-
-print("!23")
+# d2l.train_ch3(net,train_iter, test_iter, loss, num_epochs,trainer)
 
 
-print("!23")
-
-print("!23")
-
-print("!23")
-
+print("123")
